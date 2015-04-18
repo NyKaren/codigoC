555125e7e9540d67c035eda2556a89ccadf70ea6
NyKaren <neydekaren@infojr.com.br>
2015-04-18 15:37:26 -0300

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+Codigos de apredizagem C
+=================
---
+++ soma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int a,b,c,d,e,f;
+int main () {
+//printf("Digite um número de a\n");
+scanf("%d", &a);
+//printf("Digite um número de b\n");
+scanf("%d", &b);
+c=a+b;
+printf("%d\n", c);
+return 0;
+}
